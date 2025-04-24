8eab908fdc6295dea87f65e1ac68d01b09a0b110
Paul Chavard <pro@paul.chavard.net>
2025-04-24 13:52:06 +0200

    in develpment generate openapi json

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,13 +1,20 @@
 import { Scalar } from '@scalar/hono-api-reference';
-import { openAPISpecs } from 'hono-openapi';
+import { generateSpecs, openAPISpecs } from 'hono-openapi';
 import { showRoutes } from 'hono/dev';
 import { start } from '../services/boss';
+import { env } from '../services/env';
 import { app } from './app';
 
 app.get('/openapi', openAPISpecs(app));
 app.get('/api/docs', Scalar({ theme: 'saturn', url: '/openapi' }));
 
 await start();
-showRoutes(app);
+
+if (env.NODE_ENV == 'development') {
+  generateSpecs(app).then((spec) => {
+    Bun.write('openapi.json', JSON.stringify(spec, null, 2));
+  });
+  showRoutes(app);
+}
 
 export default app;
