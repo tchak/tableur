192832f65f2d2ebfd3ddbee8742225a9169df7a6
Paul Chavard <pro@paul.chavard.net>
2025-05-16 11:17:34 +0200

    generate openapi spec only for the api

--- app/server/index.ts
+++ app/server/index.ts
@@ -9,16 +9,18 @@ import { createHonoServer } from 'react-router-hono-server/bun';
 import { router } from '~/services/storage';
 import { api } from './api';
 
+const isDev = import.meta.env.MODE == 'development';
+
 export default await createHonoServer({
   configure(app) {
     app.route('api/v1', api);
     app.route('storage', router);
 
-    app.get('api/schema', openAPISpecs(app));
+    app.get('api/schema', openAPISpecs(api));
     app.get('api/docs', Scalar({ theme: 'saturn', url: '/api/schema' }));
 
-    if (import.meta.env.MODE == 'development') {
-      generateSpecs(app).then((spec) => {
+    if (isDev) {
+      generateSpecs(api).then((spec) => {
         Bun.write('openapi.json', JSON.stringify(spec, null, 2));
       });
       showRoutes(app);
@@ -27,8 +29,7 @@ export default await createHonoServer({
   beforeAll(app) {
     app.use(timing());
     app.use(requestId());
-
-    if (import.meta.env.MODE == 'development') {
+    if (isDev) {
       app.use('api/v1', prettyJSON());
     }
   },
