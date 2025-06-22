774c46eb0b392d0064830939746dfc781a9b28be
Paul Chavard <pro@paul.chavard.net>
2025-06-22 17:21:31 +0200

    improve expression

--- app/core/expression.ts
+++ app/core/expression.ts
@@ -3,26 +3,56 @@ import * as v from 'valibot';
 
 import { ID, TypedValue, type Data } from './shared.contract';
 
-const Operator = v.picklist(['eq', 'ne', 'lt', 'gt', 'lte', 'gte']);
+const BinaryOperator = v.picklist([
+  'eq',
+  'ne',
+  'lt',
+  'gt',
+  'lte',
+  'gte',
+  'in',
+  'nin',
+  'has',
+]);
+const UnaryOperator = v.picklist(['present', 'blank']);
+
+const UnaryExpression = v.object({
+  type: v.literal('unary'),
+  left: v.object({ id: ID }),
+  operator: UnaryOperator,
+});
 
 const BinaryExpression = v.object({
   type: v.literal('binary'),
   left: v.object({ id: ID }),
-  operator: Operator,
+  operator: BinaryOperator,
   right: TypedValue,
+});
+
+const _Expression = v.variant('type', [UnaryExpression, BinaryExpression]);
+
+const _AndExpression = v.object({
+  type: v.literal('and'),
+  expressions: v.array(_Expression),
+});
+
+const _OrExpression = v.object({
+  type: v.literal('or'),
+  expressions: v.array(_Expression),
 });
 
 const AndExpression = v.object({
   type: v.literal('and'),
-  expressions: v.array(BinaryExpression),
+  expressions: v.array(v.variant('type', [_Expression, _OrExpression])),
 });
 
 const OrExpression = v.object({
   type: v.literal('or'),
-  expressions: v.array(BinaryExpression),
+  expressions: v.array(v.variant('type', [_Expression, _AndExpression])),
 });
 
 export const Expression = v.variant('type', [
+  UnaryExpression,
   BinaryExpression,
   AndExpression,
   OrExpression,
@@ -31,12 +61,22 @@ export type Expression = v.InferOutput<typeof Expression>;
 
 export function compute(expression: Expression, data: Data): boolean {
   switch (expression.type) {
+    case 'unary': {
+      const left = data[expression.left.id];
+      if (expression.operator == 'present') {
+        return left != null;
+      }
+      return left == null;
+    }
     case 'binary': {
       const left = data[expression.left.id];
       const right = expression.right;
       if (left?.type != right.type) {
         return expression.operator == 'ne' ? true : false;
-      } else if (left.type == 'datetime' && right.type == 'datetime') {
+      } else if (
+        (left.type == 'datetime' || left.type == 'date') &&
+        (right.type == 'datetime' || right.type == 'date')
+      ) {
         const result = compareDateTime(
           Temporal.PlainDateTime.from(left.value),
           Temporal.PlainDateTime.from(right.value),
@@ -54,6 +94,10 @@ export function compute(expression: Expression, data: Data): boolean {
             return result == 'lt' || result == 'eq';
           case 'gte':
             return result == 'gt' || result == 'eq';
+          default:
+            throw new Error(
+              `Invalid operator: ${expression.operator} for ${left.type}`,
+            );
         }
       } else if (left.type == 'number' && right.type == 'number') {
         switch (expression.operator) {
@@ -69,7 +113,37 @@ export function compute(expression: Expression, data: Data): boolean {
             return left.value <= right.value;
           case 'gte':
             return left.value >= right.value;
+          default:
+            throw new Error(
+              `Unsupported operator ${expression.operator} for type ${left.type}`,
+            );
         }
+      } else if (left.type == 'choiceList' && right.type == 'choice') {
+        if (expression.operator == 'has') {
+          return left.value.includes(right.value);
+        }
+        throw new Error(
+          `Unsupported operator ${expression.operator} for type ${left.type}`,
+        );
+      } else if (left.type == 'choice') {
+        if (right.type == 'choice') {
+          switch (expression.operator) {
+            case 'eq':
+              return left.value == right.value;
+            case 'ne':
+              return left.value != right.value;
+          }
+        } else if (right.type == 'choiceList') {
+          switch (expression.operator) {
+            case 'in':
+              return right.value.includes(left.value);
+            case 'nin':
+              return !right.value.includes(left.value);
+          }
+        }
+        throw new Error(
+          `Unsupported operator ${expression.operator} for type ${left.type}`,
+        );
       }
       switch (expression.operator) {
         case 'eq':
